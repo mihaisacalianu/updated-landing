d09df75ba61f0176e0f7b57cb78c325f589092fe
mihaisacalianu <mihaisacalianu@gmail.com>
2025-05-12 16:17:05 +0100

    fixed width of nav when on mobile to 100% and when home btn from sidenav is click it goes all the way to header rather than just about section

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,8 +2,8 @@ import { HashLink } from 'react-router-hash-link';
 
 function Nav() {
   return (
-    <nav aria-label="Main Navigation"
-    className='bg-[#131315] text-[#A88AF0] text-[1.2rem] sm:text-[1.8rem] p-4 mt-4 rounded-full w-[80%] sm:w-[60%]: sm:p-2 md:w-[40%] mx-auto text-center'>
+    <nav id='header' aria-label="Main Navigation"
+    className='bg-[#131315] text-[#A88AF0] text-[1.2rem] sm:text-[1.8rem] p-4 mt-4 rounded-full w-[100%] sm:w-[60%]: sm:p-2 md:w-[40%] mx-auto text-center'>
       <ul className='flex gap-5 sm:gap-10 w-full justify-center items-center'>
         <li><HashLink smooth to="#skills">Skills</HashLink></li>
         <li><HashLink smooth to="#projects">Projects</HashLink></li>
--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -9,7 +9,7 @@ function SideNav() {
   return (
     <nav className='sticky top-10 left-0 sm:w-[60px] sm:ml-4 sm:p-2 sm:bg-[#131315] rounded-xl flex items-center sm:justify-center z-10'>
       <ul className='text-4xl text-[#A88AF0] flex flex-col gap-3 '>
-        <li ><HashLink smooth to="#about"><GoHomeFill /></HashLink></li>
+        <li ><HashLink smooth to="#header"><GoHomeFill /></HashLink></li>
         <li><HashLink smooth to="#skills"><FaTools/></HashLink></li>
         <li><HashLink smooth to="#projects"><IoFolderOpen /></HashLink></li>
         <li><HashLink smooth to="#education"><IoSchoolSharp /></HashLink></li>
